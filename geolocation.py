1abac9e690507925f78b76ac8682cfe8166cb285
npersall <npersall@gmail.com>
2020-06-08 17:31:03 -0700

    Fix Bug and removed testing key

--- geolocation.py
+++ geolocation.py
@@ -2,7 +2,7 @@ import httplib2
 import json
 
 def getGeoLocation(inputString):
-    google_api_key = "AIzaSyCijUeimMV3MPDc_4jX4Q_zHQKjRdS60p0"
+    google_api_key = "API_KEY"
     locationString = inputString.replace(" ", "+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
     h = httplib2.Http()
