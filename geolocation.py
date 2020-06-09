ea20e61a5da43c1683eb05ba035bd4544aeffa12
npersall <npersall@gmail.com>
2020-06-08 17:18:39 -0700

    Fix Bug

--- geolocation.py
+++ geolocation.py
@@ -2,11 +2,12 @@ import httplib2
 import json
 
 def getGeoLocation(inputString):
-    google_api_key = "API_KEYS"
+    google_api_key = "AIzaSyCijUeimMV3MPDc_4jX4Q_zHQKjRdS60p0"
     locationString = inputString.replace(" ", "+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
     h = httplib2.Http()
-    result = json.loads(h.request(url,'GET')[1])
-    latitude = result['results'][0]['geometry']['location']['lat']
-    longitude = result['results'][0]['geometry']['location']['lng']
-    return (latitude,longitude)
+    resonse, content = h.request(url, 'GET')
+    result = json.loads(content)
+    latitude = result['results'] [0] ['geometry'] ['location']['lat']
+    longitude = result['results'] [0] ['geometry'] ['location']['lng']
+    return (latitude, longitude)
